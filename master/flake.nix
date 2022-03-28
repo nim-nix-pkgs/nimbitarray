3137c475fc8fff809083b16aa575a1be703a5b57
hugosenari <hugosenari@gmail.com>
2022-03-27 21:52:58 -0300

    chore: re index nimbitarray

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-bitarray-master.ref   = "refs/heads/master";
   inputs.src-bitarray-master.owner = "YesDrX";
   inputs.src-bitarray-master.repo  = "bitarray";
-  inputs.src-bitarray-master.dir   = "";
   inputs.src-bitarray-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
